f8ee1e14dc0a9c9857addf90dd5485fe1dc47e9d
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-09-24 15:25:37 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = """Hello <i><b>{}</b></i>,
 
-This is a Telegram Rename clone of <a href='https://t.me/renamer23_bot'>Renamer ⚡</a> by {}
+This is a Telegram Rename clone of <a href='https://t.me/turboremaxbot'>Turbo Renamer ⚡</a> by {}
 
 I Can rename ✍ with custom thumbnail and upload as video/file
 
@@ -9,25 +9,25 @@ Type /help for more details."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
-    DOWNLOAD_START_VIDEO = "Downloading to my server.....📥"
-    DOWNLOAD_START = "Downloading to my server.....📥"
-    UPLOAD_START_VIDEO = "Uploading as video.....📤"
-    UPLOAD_START = "Uploading as File.....📤"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Anonymous](https://t.me/Ns_AnoNymouS)'s bot.**"
+    DOWNLOAD_START_VIDEO = "Downloading to Turbo Server.....📥🐅🐅"
+    DOWNLOAD_START = "Downloading to Turbo Server.....📥🔥🔥"
+    UPLOAD_START_VIDEO = "Uploading as Video.....📤⚡⚡"
+    UPLOAD_START = "Uploading as File.....📤🌹🌹"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Turbo Renamer](https://t.me/avatharsink)'s bot.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/anonymousbotdiscussion'>Ns Bot Supporters</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved ✅️ permanently. This image will be used in the File."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "@renamer23_bot"
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/avatharsink'>Turbo Renamer</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved 🤗 permanently. This image will be used in the File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "🤔 Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "🤨 Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "🤫 Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = "Share and Support As \n@SerialCoIn"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "😠 No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """Hai <b><i>{}</i></b>, 
+    HELP_USER = """Vanakkam <b><i>{}</i></b>, 
 
-I am Renamer bot ✍ by <a href='https://t.me/{}'>My Father 👨‍🏫</a>
+I am Turbo Renamer bot ✍ by <a href='https://t.me/{}'>Avathar Gik</a>
     
 1. Send Me A Thumbnail.
 
@@ -38,13 +38,13 @@ I am Renamer bot ✍ by <a href='https://t.me/{}'>My Father 👨‍🏫</a>
 4. Reply to that message with <code>/rename_vidoe new name.extension</code>. with custom thumbnail support.\n(uploading as Video)
 
    
-<b>Thanks to <i><a href="https://t.me/Ns_AnoNymous">Anonymous 👨‍🏫</a></i> for his source code. check /about for source code</b>
+<b>Thanks to <i><a href="https://t.me/avatharsink">Turbo Renamer 🙇</a></i> for his source code. check /about for source code</b>
 
 --------
 
-Support Group : @anonymousbotdiscussion"""
-    REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
-    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🤦‍♂️ Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
+Support Group : @SerialCoIn"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "🙆 Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🙋 Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
@@ -53,7 +53,7 @@ Free users only 1 request per 30 minutes.
 The given file name has {num} characters.
 
 <b>Essays Not allowed in Telegram file name!</b>
-©️ <code>@renamer23_bot</code>
+©️ <code>@turboremaxbot</code>
 Please short your file name and try again!"""
 
     About = """Hi __{}__,
@@ -61,10 +61,10 @@ Please short your file name and try again!"""
 
 **🧰 Framework:** Pyrogram
 
-**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)
+**👨‍💻 Developer:** [Turbo Renamer](https://t.me/turboremaxbot)
 
-📮 Channel: [INFOTEL](https://t.me/InFoTel14)
+📮 Channel: [Serial](https://t.me/SerialCoIn)
 
-**👥 Group:** [INFOTEL GROUP](https://t.me/InFoTelGroup)
+**👥 Group:** [Tamil Serial Group](https://t.me/SerialCoIng)
 
-**💻 Source Code:**[Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"""
+**💻 Source Code:**[Press Me](https://github.com/Empty)"""
