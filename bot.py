7854f1d3f922b2d58db76611d8bb7b3c68dd5aa6
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-09-27 00:40:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(1370501629)
+    Config.AUTH_USERS.add(880087645)
     app.run()
