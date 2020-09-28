7e28ba4695b8881868121c78ce9c1b328607d7d1
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-09-29 01:38:14 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Turbo Renamer
+# (c) Shrimadhav U K
 
 # the logging things
 import logging
@@ -31,28 +31,18 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
+from database.database import *
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["rename_video"]))
 async def rename_video(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+        await update.reply_text("You are B A N N E D")
         return
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 128:
-            await update.reply_text(
-                Translation.IFLONG_FILE_NAME.format(
-                    alimit="128",
-                    num=len(file_name)
-                )
-            )
-            return
+        new_name = file_name[:60] + file_name[-4:]
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         b = await bot.send_message(
@@ -71,7 +61,7 @@ async def rename_video(bot, update):
                 c_time
             )
         )
-        if the_real_download_location is not None:
+        if (the_real_download_location is not None) and (len(file_name) > 64):
             try:
                 await bot.edit_message_text(
                     text=Translation.SAVED_RECVD_DOC_FILE,
@@ -80,19 +70,26 @@ async def rename_video(bot, update):
                 )
             except:
                 pass
-            new_file_name = download_location + file_name
+            new_file_name = download_location + new_name
             os.rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.chat.id,
                 message_id=b.message_id
                 )
-
             logger.info(the_real_download_location)
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
-                thumb_image_path = None
+                mes = await get_thumb(update.from_user.id)
+                if mes != None:
+                    m = await bot.get_messages(update.chat.id, mes.msg_id)
+                    await m.download(file_name=thumb_image_path)
+                    thumb_image_path = thumb_image_path
+                else:
+                    thumb_image_path = None
             else:
+                width = 0
+                height = 0
                 metadata = extractMetadata(createParser(thumb_image_path))
                 if metadata.has("width"):
                     width = metadata.get("width")
@@ -112,11 +109,12 @@ async def rename_video(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video=new_file_name,
-              #  duration=duration,
-                caption=f"<b>{file_name} \n\nShare and Support\n\n@SerialCoIn</b>",
-                supports_streaming=True,
-                # reply_markup=reply_markup,
+               # width=width,
+                #height=height,
+                duration=duration,
                 thumb=thumb_image_path,
+                caption=description.format(file_name[:-4]),
+                # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
@@ -127,7 +125,80 @@ async def rename_video(bot, update):
             )
             try:
                 os.remove(new_file_name)
-                #os.remove(thumb_image_path)
+                os.remove(thumb_image_path)
+            except:
+                pass
+            await bot.edit_message_text(
+                text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                chat_id=update.chat.id,
+                message_id=b.message_id,
+                disable_web_page_preview=True
+            )
+        if (the_real_download_location is not None) and (len(file_name) <= 64):
+            try:
+                await bot.edit_message_text(
+                    text=Translation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=b.message_id
+                )
+            except:
+                pass
+            new_file_name = download_location + file_name
+            os.rename(the_real_download_location, new_file_name)
+            await bot.edit_message_text(
+                text=Translation.UPLOAD_START,
+                chat_id=update.chat.id,
+                message_id=b.message_id
+                )
+            logger.info(the_real_download_location)
+            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+            if not os.path.exists(thumb_image_path):
+                mes = await get_thumb(update.from_user.id)
+                if mes != None:
+                    m = await bot.get_messages(update.chat.id, mes.msg_id)
+                    await m.download(file_name=thumb_image_path)
+                    thumb_image_path = thumb_image_path
+                else:
+                    thumb_image_path = None
+            else:
+                width = 0
+                height = 0
+                metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("width"):
+                    width = metadata.get("width")
+                if metadata.has("height"):
+                    height = metadata.get("height")
+                # resize image
+                # ref: https://t.me/PyrogramChat/44663
+                # https://stackoverflow.com/a/21669827/4723940
+                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
+                img = Image.open(thumb_image_path)
+                # https://stackoverflow.com/a/37631799/4723940
+                # img.thumbnail((90, 90))
+                img.resize((320, height))
+                img.save(thumb_image_path, "JPEG")
+                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+            c_time = time.time()
+            await bot.send_video(
+                chat_id=update.chat.id,
+                video=new_file_name,
+                #height=height,
+                #width=width,
+                #duration=duration,
+                thumb=thumb_image_path,
+                caption=description.format(file_name[:-4]),
+                # reply_markup=reply_markup,
+                reply_to_message_id=update.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    Translation.UPLOAD_START,
+                    b, 
+                    c_time
+                )
+            )
+            try:
+                os.remove(new_file_name)
+                os.remove(thumb_image_path)
             except:
                 pass
             await bot.edit_message_text(
@@ -139,6 +210,6 @@ async def rename_video(bot, update):
     else:
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
+            text=Translation.REPLY_TO_DOC_FOR_RENAME_VIDEO,
             reply_to_message_id=update.message_id
         )
