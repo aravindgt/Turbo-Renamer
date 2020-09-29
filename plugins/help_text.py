bc429d1d461fbef4dd3dd158a9d7a9755181a431
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-09-29 07:13:24 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -25,7 +25,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-myfather = 'https://t.me/{}'.format(Config.USER_NAME[1:])
+myfather = 'https://t.me/JAsuran123'.format(Config.USER_NAME[1:])
 
 
 
